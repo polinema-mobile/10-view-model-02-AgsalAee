1669b6fe838fbe9a1f937d9bc39579977274f14a
AgsalAee <agsal.agsal9@gmail.com>
2020-11-05 21:20:51 +0700

    buat splash saja

---
+++ app/src/main/java/id/putraprima/mvvmlogin/models/User.java
@@ -0,0 +1,26 @@
+package id.putraprima.mvvmlogin.models;
+
+public class User {
+    private String nama,password;
+
+    public User(String nama, String password) {
+        this.nama = nama;
+        this.password = password;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
